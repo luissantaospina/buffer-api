230546c1811f2694b549a1516958d0307e1af773
Luis <luisfernandosantaospina@gmail.com>
2023-03-30 18:49:06 -0500

    Add enum

---
+++ app/enums/policy_enum.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class PolicyEnum(Enum):
+    FIFO = 'FIFO'
+    LIFO = 'LIFO'
--- app/services/buffer/buffer_service.py
+++ app/services/buffer/buffer_service.py
@@ -2,6 +2,7 @@ from app.services.buffer.buffer import Buffer
 from app.services.buffer.readers.buffer_reader import BufferReader
 from app.services.buffer.readers import ReaderFifo, ReaderLifo
 from app.data_transfer_objects import MessageDTO, PolicyDTO
+from app.enums import PolicyEnum
 
 
 def get_all_buffer() -> Buffer:
@@ -16,9 +17,9 @@ def insert_item_to_buffer(message_to_insert: MessageDTO) -> None:
 
 def extract_item_to_buffer(policy: PolicyDTO) -> None:
     current_buffer = Buffer().get_buffer()
-    if policy.method == 'FIFO' and current_buffer:
+    if policy.method == PolicyEnum.FIFO.value and current_buffer:
         buffer_reader = BufferReader(ReaderFifo())
         buffer_reader.reader(current_buffer)
-    if policy.method == 'LIFO' and current_buffer:
+    if policy.method == PolicyEnum.LIFO.value and current_buffer:
         buffer_reader = BufferReader(ReaderLifo())
         buffer_reader.reader(current_buffer)

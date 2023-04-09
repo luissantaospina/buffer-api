965cd4356a83c00c648fea52ae5b3eb03c6730a2
Luis <luisfernandosantaospina@gmail.com>
2023-04-09 06:43:23 -0500

    Change name to test

--- app/tests/readers_test.py
+++ app/tests/readers_test.py
@@ -9,7 +9,7 @@ class TestReader(unittest.TestCase):
         reader.resolve(buffer_test)
         self.assertEqual(buffer_test, [2, 3, 4, 5])
 
-    def test_resolve_removes_first_item_from_buffer_lifo(self):
+    def test_resolve_removes_last_item_from_buffer_lifo(self):
         reader = ReaderLifo()
         buffer_test = [1, 2, 3, 4, 5]
         reader.resolve(buffer_test)

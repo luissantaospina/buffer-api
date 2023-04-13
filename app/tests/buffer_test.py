9064c05c584ac23e29ef301049bd438c21da83f8
Luis <luisfernandosantaospina@gmail.com>
2023-04-13 07:35:54 -0500

    Fix the test

--- app/tests/buffer_test.py
+++ app/tests/buffer_test.py
@@ -1,22 +1,41 @@
 import unittest
+import redis
 from app.data_transfer_objects import MessageDTO
-from app.services.buffer.singleton import Buffer
+from app.services.buffer.i_buffer.impl import ReaderFifo
+from app.services.buffer.readers import BufferReader
+from app.repositories.buffer_repository import \
+    get_all_buffer_repository,\
+    insert_item_to_buffer_repository,\
+    extract_item_to_buffer_repository
 
 
 class TestBuffer(unittest.TestCase):
     def setUp(self):
-        self.buffer_instance1 = Buffer()
-        self.buffer_instance2 = Buffer()
+        redis_cache = redis.Redis(host='localhost', port=6379, db=0)
+        redis_cache.flushall()
 
-    def test_singleton(self):
-        self.assertIs(self.buffer_instance1, self.buffer_instance2)
+    def test_insert_item_to_buffer_repository(self):
+        message_to_insert = MessageDTO(body=1)
+        result = insert_item_to_buffer_repository(message_to_insert)
+        self.assertEqual(result.body, 1)
 
-    def test_set_get_buffer(self):
-        message1 = MessageDTO("test message 1")
-        message2 = MessageDTO("test message 2")
+    def test_get_all_buffer_repository(self):
+        cached_buffer = get_all_buffer_repository()
+        self.assertEqual(cached_buffer, [])
 
-        self.buffer_instance1.set_buffer(message1)
-        self.buffer_instance2.set_buffer(message2)
+        message_to_insert = MessageDTO(body=1)
+        insert_item_to_buffer_repository(message_to_insert)
 
-        self.assertListEqual(self.buffer_instance1.get_buffer(), [message1, message2])
-        self.assertListEqual(self.buffer_instance2.get_buffer(), [message1, message2])
+        cached_buffer = get_all_buffer_repository()
+        self.assertEqual(cached_buffer, [1])
+
+    def test_extract_item_to_buffer_repository(self):
+        message_to_insert = MessageDTO(body=1)
+        insert_item_to_buffer_repository(message_to_insert)
+
+        buffer_reader = BufferReader(ReaderFifo())
+        item_extracted = extract_item_to_buffer_repository(buffer_reader)
+        self.assertEqual(item_extracted, 1)
+
+        cached_buffer = get_all_buffer_repository()
+        self.assertEqual(cached_buffer, [])
